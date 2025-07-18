2443a3d0600d68b53bf0f3bc6b174b9606df36be
Ashwinishree M <ashwinishreem@gmail.com>
2025-07-18 15:35:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,18 @@ from auth import show_login_page, show_signup_page, get_current_user, logout
 from sketch import show_sketch_page
 from user_history import show_history_page
 
+def load_css():
+    with open("style.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
 def main():
     st.set_page_config(page_title="Imagination Sketch", layout="centered")
+    load_css()
 
     if "page" not in st.session_state:
         st.session_state.page = "login"
 
-    st.title("Imagination Sketch")
+    st.markdown("<div class='main-title'>Imagination Sketch</div>", unsafe_allow_html=True)
 
     user = get_current_user()
 
@@ -31,8 +36,6 @@ def main():
             show_login_page()
         else:
             show_signup_page()
-
-
 
 if __name__ == "__main__":
     main()
